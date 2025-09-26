7f33e3027e7bec987591b858251e48a327789dee
bellalicerestaurante-code <bellalicerestaurante@gmail.com>
2025-09-25 23:23:22 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração padrão do Vite
+// Configuração do Vite para React
 export default defineConfig({
   plugins: [react()],
   build: {
